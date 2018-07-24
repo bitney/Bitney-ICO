131cb4bed260c8c8d8d4eab6e7ae80cf2f6be9cf
Bitney <38420761+bitney@users.noreply.github.com>
2018-07-24 11:38:34 +0800

    Create BTNYBurnable.sol

---
+++ Contract/BTNYBurnable.sol
@@ -0,0 +1,23 @@
+pragma solidity 0.4.19;
+
+import "./BTNYSale.sol";
+
+contract BTNYBurnable {
+    event Burn(address indexed burner, uint256 value);
+
+    /**
+    * @dev Burns a specific amount of tokens.
+    * @param _value The amount of token to be burned.
+    */
+    function burn(uint256 _value) public onlyOwner {
+        require(_value <= balances[msg.sender]);
+        // no need to require value <= totalSupply, since that would imply the
+        // sender's balance is greater than the totalSupply, which *should* be an assertion failure
+
+        address burner = msg.sender;
+        balances[burner] = balances[burner].sub(_value);
+        _totalSupply = _totalSupply.sub(_value);
+        Burn(burner, _value);
+        Transfer(burner, owner, _value);
+    }
+}
